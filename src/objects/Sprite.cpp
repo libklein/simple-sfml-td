7b79bd4ed7444eb6f7c69436a2a2218ed5434820
Patrick Klein <patrick@libklein.com>
2020-03-22 18:07:48 +0100

    Refactor Sprite class.
    Sprite now does not inherit from sf::Sprite anymore but implements nessesary functionality by itself.
    For this purpose, it comprises a location and an array of sf::Vertices.

--- src/objects/Sprite.cpp
+++ src/objects/Sprite.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Sprite.hpp"
+#include <SFML/Graphics/RenderTarget.hpp>
 
 auto Sprite::constructFromPrototype() const -> std::unique_ptr<Sprite> {
     return std::make_unique<Sprite>(*this);
@@ -12,5 +13,87 @@ auto Sprite::FromJSON(const nlohmann::json &json, const TextureAtlas &textures) 
     return Sprite(textures.Texture(), textures.getCoord<int>(json["sprite_id"]), json["uuid"]);
 }
 
-Sprite::Sprite(const sf::Texture &texture, const sf::IntRect &rectangle, size_t id) : sf::Sprite(texture, rectangle),
-                                                                                      id_(id) {}
+Sprite::Sprite(const sf::Texture &texture, const sf::IntRect &rectangle, size_t id)
+    : texture_(&texture)
+    , id_(id) {
+    setTextureRect(rectangle);
+}
+
+void Sprite::draw(sf::RenderTarget &target, sf::RenderStates states) const {
+    states.transform *= location_.getTransform();
+    states.texture = texture_;
+    target.draw(vertices_.data(), 4, sf::Quads, states);
+}
+
+auto Sprite::getLocalBounds() const -> sf::FloatRect {
+    float width = vertices_[1].position.x - vertices_[0].position.x;
+    float height = vertices_[2].position.y - vertices_[0].position.y;
+    return {0.0, 0.0, width, height};
+}
+
+void Sprite::setPosition(float x, float y) {
+    location_.setPosition(x, y);
+}
+
+void Sprite::setPosition(const sf::Vector2f &position) {
+    location_.setPosition(position);
+}
+
+auto Sprite::getPosition() const -> const sf::Vector2f & {
+    return location_.getPosition();
+}
+
+void Sprite::setTextureRect(const sf::IntRect &rect) {
+    auto left = static_cast<float>(rect.left), width = static_cast<float>(rect.width);
+    auto top = static_cast<float>(rect.top), height = static_cast<float>(rect.height);
+
+    vertices_[0].texCoords = {left, top};
+    vertices_[1].texCoords = {left + width, top};
+    vertices_[2].texCoords = {left + width, top + height};
+    vertices_[3].texCoords = {left, top + height};
+
+    vertices_[0].position = {0.0, 0.0};
+    vertices_[1].position = {width, 0.0};
+    vertices_[2].position = {width, height};
+    vertices_[3].position = {0.0, height};
+}
+
+Location::Location(const sf::Vector2f &position, float rotation) : position_(position), rotation_(rotation) {}
+
+void Location::setPosition(float x, float y) {
+    position_ = {x, y};
+    mark_dirty();
+}
+
+void Location::setPosition(const sf::Vector2f &pos) {
+    position_ = pos;
+    mark_dirty();
+}
+
+void Location::setRotation(float angle) {
+    rotation_ = angle;
+    mark_dirty();
+}
+
+auto Location::getPosition() const -> const sf::Vector2f & {
+    return position_;
+}
+
+auto Location::getTransform() const -> const sf::Transform & {
+    if(transform_needs_update_) recompute_transformation();
+    return cached_transform_;
+}
+
+void Location::mark_dirty() const {
+    transform_needs_update_ = true;
+}
+
+void Location::recompute_transformation() const {
+    cached_transform_ = sf::Transform::Identity;
+    cached_transform_.translate(getPosition()).rotate(getRotation());
+    transform_needs_update_ = false;
+}
+
+auto Location::getRotation() const -> float {
+    return rotation_;
+}
--- src/objects/Sprite.hpp
+++ src/objects/Sprite.hpp
@@ -11,16 +11,61 @@
 #include <TextureAtlas.hpp>
 #include <memory>
 
-class Sprite : public sf::Sprite {
+class Location {
+protected:
+    sf::Vector2f position_ = {0., 0.};
+    float rotation_ = 0.;
+private:
+    mutable sf::Transform cached_transform_ = sf::Transform::Identity;
+    mutable bool transform_needs_update_ = false;
+public:
+    Location() = default;
+    Location(const sf::Vector2f &position, float rotation);
+
+    // Setters
+    void setRotation(float angle);
+    void setPosition(float x, float y);
+    void setPosition(const sf::Vector2f &position);
+
+    // Getters
+    [[nodiscard]] auto getRotation() const -> float;
+    [[nodiscard]] auto getPosition() const -> const sf::Vector2f &;
+
+    // Computed
+    [[nodiscard]] auto getTransform() const -> const sf::Transform&;
+
+protected:
+    void mark_dirty() const;
+    void recompute_transformation() const;
+};
+
+class Sprite : public sf::Drawable {
+protected:
+    Location location_;
+    std::array<sf::Vertex, 4> vertices_;
+    const sf::Texture *texture_;
+private:
     std::size_t id_;
 public:
     Sprite(const sf::Texture &texture, const sf::IntRect &rectangle, size_t id);
 
-    virtual auto constructFromPrototype() const -> std::unique_ptr<Sprite>;
+    [[nodiscard]] virtual auto constructFromPrototype() const -> std::unique_ptr<Sprite>;
 
-    auto SpriteID() const -> auto { return id_; }
+    [[nodiscard]] auto SpriteID() const -> auto { return id_; }
 
-    static auto FromJSON(const nlohmann::json &json, const TextureAtlas &textures) -> Sprite;
+    [[nodiscard]] static auto FromJSON(const nlohmann::json &json, const TextureAtlas &textures) -> Sprite;
+
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
+
+    // Setters
+    void setPosition(float x, float y);
+    void setPosition(const sf::Vector2f &position);
+    void setTextureRect(const sf::IntRect &rect);
+
+    // Getters
+    [[nodiscard]] auto getLocalBounds() const -> sf::FloatRect;
+    [[nodiscard]] auto getPosition() const -> const sf::Vector2f &;
+
 };
 
 
